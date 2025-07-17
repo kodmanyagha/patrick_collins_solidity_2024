df314f53fd6ef571502080a4f48cc9324ddcc235
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2025-07-17 09:55:26 +0300

    init

---
+++ README.md
@@ -0,0 +1,55 @@
+## Solidity Tutorial
+
+Tutorial video with time: https://youtu.be/-1GB6m39-rM?t=17205
+
+## Usage
+
+### Build
+
+```shell
+$ forge build
+```
+
+### Test
+
+```shell
+$ forge test
+```
+
+### Format
+
+```shell
+$ forge fmt
+```
+
+### Gas Snapshots
+
+```shell
+$ forge snapshot
+```
+
+### Anvil
+
+```shell
+$ anvil
+```
+
+### Deploy
+
+```shell
+$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
+```
+
+### Cast
+
+```shell
+$ cast <subcommand>
+```
+
+### Help
+
+```shell
+$ forge --help
+$ anvil --help
+$ cast --help
+```
---
+++ src/SimpleStorage.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 public favoriteNumber;
+    uint256[] favoriteNumbers;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person public pat = Person(10, "Pat");
+    Person public john = Person({favoriteNumber: 20, name: "John"});
+
+    Person[] public persons;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        persons.push(Person({favoriteNumber: _favoriteNumber, name: _name}));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function emptyYelloFn() public {
+        //
+    }
+}
